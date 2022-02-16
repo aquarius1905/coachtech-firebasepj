90ba1108ba3edcd5f25f5fb9d5f30df72c970f8e
reiko_iguchi <reiko6122@gmail.com>
2022-02-16 10:31:28 +0900

    update

---
+++ pages/logout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="logout">
+    <button @click="logout">ログアウト</button>
+    <NuxtLink to="/">戻る</NuxtLink>
+  </div>
+</template>
+
+<script>
+import firebase from '~/plugins/firebase'
+export default {
+  methods: {
+    logout() {
+      firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        alert('ログアウトが完了しました')
+        this.$router.replace('/');
+      })
+    },
+  },
+}
+</script>
