c78d4304aa753333f64883e524ec1b7a26ec833a
reiko_iguchi <reiko6122@gmail.com>
2022-02-16 14:16:12 +0900

    update

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="login">
     <label >
-      メールアドレス：<input v-mode="email" type="email" required />
+      メールアドレス：<input v-model="email" type="email" required />
     </label>
     <br/>
     <label >
-      パスワード：<input v-mode="password" type="password" required />
+      パスワード：<input v-model="password" type="password" required />
     </label>
     <br/>
     <button @click="login">ログイン</button>
--- pages/register.vue
+++ pages/register.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="register">
     <label >
-      メールアドレス：
-      <input v-model="email" type="email" required />
+      メールアドレス：<input v-model="email" type="email" required />
     </label>
     <br/>
     <label >
-      パスワード：
-      <input v-model="password" type="password" required />
+      パスワード：<input v-model="password" type="password" required />
     </label>
     <br/>
     <button @click="register">新規登録</button>
